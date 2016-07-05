341f052e667f030a5e5a5f12eb3cd6d345e3c114
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-07-05 12:41:32 +0200

    Add basic middleware code

---
+++ Sources/TemplateDisplay/TemplateDisplay.swift
@@ -0,0 +1,38 @@
+//
+//  TemplateDisplay.swift
+//  
+//
+//  Created by Jeremi Kaczmarczyk on 05.07.2016.
+//
+//
+
+import Foundation
+import Kitura
+import HeliumLogger
+import LoggerAPI
+
+import Mustache
+import File
+
+struct TemplateDisplay {
+    
+    let response: RouterResponse
+    
+    func show(withPathString path: String, context: [String: String]) {
+        guard let templateFile = try? File(path: path),
+            let templateString = try? String(data: templateFile.readAllBytes()),
+            let template = try? Template(string: templateString),
+            let body = try? template.render(context: Context(box: Box(dictionary: message.context))) else {
+                Log.error("Failed to parse template")
+        }
+        
+        do {
+            Log.debug("sending webpage: \(filePath)")
+            try response.send(body.string)
+        }
+            
+        catch {
+            Log.error("Failed to send response \(error)")
+        }
+    }
+}
