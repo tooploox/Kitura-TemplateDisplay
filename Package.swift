4aeee7fdecceb8af7092697a9cb274f52dba9b31
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-07-05 11:47:17 +0200

    Init repository with required files

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "HealthCheck",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
+    ]
+)
