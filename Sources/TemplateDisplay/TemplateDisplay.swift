a331ea918c55cee2bd225bc42c30f6da61ba4bc9
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-07-07 12:40:39 +0200

    Fix optional unwrapping

--- Sources/TemplateDisplay/TemplateDisplay.swift
+++ Sources/TemplateDisplay/TemplateDisplay.swift
@@ -22,7 +22,8 @@ public struct TemplateDisplay {
     }
     
     public func show(withPathString path: String, context: [String: String]) {
-        guard let templateFile = try? NSData(contentsOfURL: NSURL(string: path)),
+        guard let url = NSURL(string: path),
+            let templateFile = try? NSData(contentsOfURL: url),
             let templateString = try? String(data: templateFile.readAllBytes()),
             let template = try? Template(string: templateString),
             let body = try? template.render(context: Context(box: Box(dictionary: context))) else {
