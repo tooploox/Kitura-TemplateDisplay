77a223babbffe6fe0621a63e7cccad816db5b163
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-07-07 15:19:39 +0200

    Use different initializers

--- Sources/TemplateDisplay/TemplateDisplay.swift
+++ Sources/TemplateDisplay/TemplateDisplay.swift
@@ -22,41 +22,14 @@ public struct TemplateDisplay {
     }
     
     public func show(withPathString path: String, context: [String: String]) {
-        guard let url = NSURL(string: path) else {
-//            let templateFile = NSData(contentsOf: url),
-//            let templateString = String(data: templateFile, encoding: NSUTF8StringEncoding),
-//            let template = try? Template(string: templateString),
-//            let body = try? template.render(context: Context(box: Box(dictionary: context))) else {
-//                return Log.error("Failed to parse template")
-            return Log.error("Failed to create url")
-
+            
+        guard let templateData = NSData(contentsOfFile: path),
+            let templateString = String(data: templateData, encoding: NSUTF8StringEncoding),
+            let template = try? Template(string: templateString),
+            let body = try? template.render(context: Context(box: Box(dictionary: message.context))) else {
+                return Log.error("Failed to parse template")
         }
-        Log.info("\(url)")
-        
-        guard let templateFile = NSData(contentsOf: url) else {
-            return Log.error("Failed to create data")
-        }
-        Log.info("\(templateFile)")
 
-        guard let templateString = String(data: templateFile, encoding: NSUTF8StringEncoding) else {
-            return Log.error("Failed to create string")
-        }
-        Log.info("\(templateString)")
-
-        
-        guard let template = try? Template(string: templateString) else {
-            return Log.error("Failed to parse template")
-        }
-        Log.info("\(template)")
-
-    
-        guard let body = try? template.render(context: Context(box: Box(dictionary: context))) else {
-            return Log.error("Failed to render template")
-        }
-        Log.info("\(body)")
-
-
-        
         do {
             Log.debug("sending webpage: \(path)")
             try response.send(body.string)
